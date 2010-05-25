4bbdc52b7d03db40cda938d9b980524dee876a74
Thomas Bertels <tbertels@gmail.com>
2010-05-25 10:28:17 +0200

    -- 1.1.1.10 20/05/2010 @ Scarlet : correction du module pour abonnements multiples et début de la période  (autodebug)

--- chrome/content/monitor/Scarlet.js
+++ chrome/content/monitor/Scarlet.js
@@ -24,6 +24,7 @@ Minimeter["Scarlet"].prototype.callback = function(step, reply) {
 			default:
 			case 1:
         this.selectProductTried = false;
+        this.selectFeatureTried = false;
 				Minimeter.http_get('http://www.scarlet.be/customercare/logon.do?username='+this.username+'&password='+this.password,this, 2);
 				break;
       case 2:
@@ -41,20 +42,21 @@ Minimeter["Scarlet"].prototype.callback = function(step, reply) {
       case "2b":
         Minimeter.http_get('http://www.scarlet.be/customercare/usage/dispatch.do', this, 3);
         break;
-      case "error":
+      /*case "error":
         var reply = decodeURIComponent(reply);
         this.reportError(step, this.name, encodeURIComponent(reply));
-        break;
+        break;*/
    		case 3:
 			  var reply = decodeURIComponent(reply);
 			  var total = /(est actuellement fixée à|verbruik staat momenteel ingesteld op|Votre limite de transfert de donnée est) <b>([0-9,.]*)\s*GB<\/b>/;
 			  var used = /<th class="digit">(.*)\s*([kMG])B<\/th>\s*<\/tr>(\s*<\/tbody>|)\s*<\/table>/;
+			  var usedZero = /instant aucune donn/; // Il n'y a pour l'instant aucune donnée pour cet abonnement. id=3498
         var regDateEnd = /(?:du|van) ([0-9]*)/;
 			  var badpage = /GESELECTEERD/;
-			  var regSelectProduct = /<option value="([0-9]*)">Scarlet ONE/;
+			  var regSelectProduct = /<option value="([0-9]*)">Scarlet (?:ONE|One)/;
 			  var regSelectFeature = /<option value="([0-9]*)">Ip Access/;
 			  
-			  if(!used.test(reply) ){
+			  if(!used.test(reply) && !usedZero.test(reply)){
           if(!badpage.test(reply)) {
             if(regSelectProduct.test(reply) && (!this.selectProductTried || (!this.selectFeatureTried && regSelectFeature.test(reply)))) {
               var selectProduct = regSelectProduct.exec(reply);
@@ -76,23 +78,27 @@ Minimeter["Scarlet"].prototype.callback = function(step, reply) {
           else
             this.reportError(step, this.name, encodeURIComponent(reply));
             //Minimeter.http_get('http://www.scarlet.be/customercare/customercare/selectbillcontract.do', this, "error");
-			  } else {
-          
+			  }
+			  else {
           if (!total.test(reply))
             this.totalVolume = 0;
           else {
             var totalValue = total.exec(reply);
             this.totalVolume = totalValue[2].replace(',','.');
       		}
-
-      		var usedValue = used.exec(reply);
-      		this.usedVolume = (usedValue[1].replace(',','.') * 1);
       		
-      		if(usedValue[2] == 'M')
-      			this.usedVolume /= 1024;
-      		else
-            if(usedValue[2] == 'k')
-              this.usedVolume /= (1024*1024);
+          if (!usedZero.test(reply)) {
+            var usedValue = used.exec(reply);
+            this.usedVolume = (usedValue[1].replace(',','.') * 1);
+            
+            if(usedValue[2] == 'M')
+              this.usedVolume /= 1024;
+            else
+              if(usedValue[2] == 'k')
+                this.usedVolume /= (1024*1024);
+          }
+          else
+            this.usedVolume = 0;
       		
       		this.usedVolume = this.usedVolume *1;
       		this.totalVolume = this.totalVolume *1;
